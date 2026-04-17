d99cfcdbb773b554d94ce5d336f236b6b6c200dd
Hai Bui <duyhai@users.noreply.github.com>
2026-04-17 15:12:32 -0700

    Add loading indicator during event creation

--- src/routes/newevent/+page.svelte
+++ src/routes/newevent/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
-
+	import { enhance } from '$app/forms';
+    let loading = false;
 </script>
 
-<form method="POST">
+<form method="POST" use:enhance={
+    async () => {
+        loading = true;
+        return async ({ update }) => {
+            await update();
+            loading = false;
+        };
+    }
+}>
     <!-- form for creating new event -->
     <label for="title">Title</label>
     <input type="text" id="title" name="title" required>
     <textarea id="description" name="description" rows="4" cols="50" placeholder="Description"></textarea>
     <label for="date">Date</label>
     <input type="datetime-local" id="date" name="date" required>
-    <button type="submit">Create Event</button>
+    <button type="submit" disabled={loading}>
+    {#if loading}
+        Loading...
+    {:else}
+        Create Event
+    {/if}
+    </button>
 </form>
